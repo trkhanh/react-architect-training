a05b8d3782c32fcd9f2e985a58257a554ac16e80
KhanhTran <khanhtran@gooappable.com>
2019-11-11 16:08:24 +0700

    update log

--- src/redux/actions/note.js
+++ src/redux/actions/note.js
@@ -1,7 +1,12 @@
-export const ADD_NOTE = 'ADD_NOTE';
-export const REMOVE_NOTE = 'REMOVE_NOTE';
+export const ADD_NOTE = "ADD_NOTE";
+export const REMOVE_NOTE = "REMOVE_NOTE";
 
 export function addNote(title, content) {
+  console.log("Action to created and  ----willDispatch()---> Dispatcher ---->", {
+    type: ADD_NOTE,
+    title: title,
+    content: content
+  });
   return { type: ADD_NOTE, title: title, content: content };
 }
 
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,21 @@
-import {createStore} from 'redux';
-import rootReducer from '../reducers/reducers';
+import { createStore } from "redux";
+import rootReducer from "../reducers/reducers";
+
+const preloadState = {
+  notes: [
+    {
+      title: "Redux uses a single store",
+      content: `Rather than locate state information in multiple
+    stores across the app, Redux apps keep everything in one place. In Flux, you can
+    have many different stores. Redux breaks from this and enforces a single global
+    store.
+    `
+    }
+  ]
+};
 
 export default createStore(
   rootReducer,
-  undefined,
+  preloadState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
